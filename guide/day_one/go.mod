1a189a11c29db56e26a8b95bb404278bde8dcf9e
Lelisayohanes <lelisayohanes1991@gmail.com>
2026-04-15 22:36:17 +0300

    all change

--- guide/day_one/go.mod
+++ guide/day_one/go.mod
@@ -2,4 +2,6 @@ module day_one
 
 go 1.26.2
 
-require github.com/labstack/echo/v5 v5.1.0 // indirect
+require github.com/labstack/echo/v5 v5.1.0
+
+require golang.org/x/time v0.14.0 // indirect
--- guide/day_one/my_first_echo.go
+++ guide/day_one/my_first_echo.go
@@ -1,20 +1,113 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 )
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+func getUser(c *echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func saveUser(c *echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, u)
+}
+
+func updateUser(c *echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "update user: "+id)
+}
+
+func deleteUser(c *echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "delete user: "+id)
+}
+
+func show(c *echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
+}
+
+func save(c *echo.Context) error {
+	name := c.FormValue("name")
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
+}
 
 func main() {
 	e := echo.New()
 
+	// Middleware
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
+
+	// Root
 	e.GET("/", func(c *echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	if err := e.Start(":1323"); err != nil {
-		e.Logger.Error("failed to start server", "error", err)
-	}
-}
+	// CRUD routes
+	e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
 
+	// Query parameters
+	e.GET("/show", show)
+
+	// Form (urlencoded + multipart)
+	e.POST("/save", save)
+
+	// Static content
+	e.Static("/static", "static")
+
+	// Group-level middleware (Basic Auth)
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(c *echo.Context, username, password string) (bool, error) {
+		return username == "joe" && password == "secret", nil
+	}))
+
+	// Route-level middleware
+	track := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c *echo.Context) error {
+			println("request to /users")
+			return next(c)
+		}
+	}
+	e.GET("/users", func(c *echo.Context) error {
+		return c.String(http.StatusOK, "/users")
+	}, track)
+
+	e.Start(":1323")
+}
